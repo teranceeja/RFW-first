de9812897db16e5fa6213679449e5d1409f4e01c
Terance Eja <terranceeja@gmail.com>
2023-11-27 12:27:42 +0000

    rest frame work1

---
+++ drflearn/views.py
@@ -0,0 +1,107 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Post
+from .serializers import PostSerializer
+
+
+# Create your views here.
+
+
+@api_view(http_method_names=['GET', 'POST'])
+def list_posts(request: Request):
+    posts = Post.objects.all()
+
+    if request.method == 'POST':
+        data = request.data
+        serializer = PostSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            response = {
+                'message': 'post created',
+                'data' : serializer.data
+            
+            }
+
+            return Response(data=response, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = PostSerializer(instance=posts, many=True)
+    response = {'message':'posts', 'data':serializer.data}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+
+@api_view(http_method_names=['GET'])
+def post_detail(request:Request, post_id:int):
+    post = get_object_or_404(Post,pk=post_id)
+    serializer = PostSerializer(instance=post)
+
+    response = {
+        "message": "post",
+        "data": serializer.data
+    }
+
+    return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=["PUT"])
+def update_post(request:Request, post_id:int):
+     post = get_object_or_404(Post, pk=post_id)
+     
+     data = request.data
+
+     serializer = PostSerializer(instance=post, data=data)
+
+     if serializer.is_valid():
+         serializer.save()
+
+         response={
+             "messages":"Post Updated Successfully",
+            "data": serializer.data
+         }
+
+         return Response(data=response, status=status.HTTP_200_OK)
+     
+     return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+   
+
+
+
+
+@api_view(http_method_names=["DELETE"])
+def delete_post(request:Request, post_id:int):
+    post = get_object_or_404(Post, pk=post_id)
+
+    post.delete()
+
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+@api_view(http_method_names=['GET', 'POST'])
+def homepage(request:Request):
+
+    if request.method == 'POST':
+        data = request.data
+        response = {'message':'Hello World', 'data':data}
+        return Response(data=response, status=status.HTTP_201_CREATED )
+    response = {'message':'Hello World'}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+
+
+
+#def homepage(request:HttpResponse):
+#    response = {"message":"Hello World"}
+ #   return JsonResponse(data = response)
+
